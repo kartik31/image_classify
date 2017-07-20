57d8f66061af7cab4a734ae2c490396ec290ab89
kartik31 <kartikagarwal31196@gmail.com>
2017-07-20 06:27:38 +0000

    ~/output_labels.txt

---
+++ dbconfig.php
@@ -0,0 +1,12 @@
+<?php
+//This script is designed by Android-Examples.com
+//Define your host here.
+$servername = "";
+//Define your database username here.
+$username = "root";
+//Define your database password here.
+$password = "abc";
+//Define your database name here.
+$dbname = "android";
+
+?>
